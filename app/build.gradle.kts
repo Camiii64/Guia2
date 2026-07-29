a19237e4c21917869c731f9a54c4a62ed06e3764
Camila Beltrán <camibeltranmedrano@gmail.com>
2026-07-28 18:40:27 -0600

    Agregando Archivos faltantes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.guia2"
+    namespace = "com.example.miperfil"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.guia2"
+        applicationId = "com.example.miperfil"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Guia2"
+rootProject.name = "MiPerfil"
 include(":app")
